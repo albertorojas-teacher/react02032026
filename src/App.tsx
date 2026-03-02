4a7666affc222d9c8a5a4556fa364890644f87c5
Alberto Rojas Gallegos <albertorojas@profesorado.cei.es>
2026-03-02 21:51:40 +0100

    feat: integrate Tailwind CSS and update App component
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated the App component to display a simple greeting instead of stock badges and plan cards.
    - Imported Tailwind CSS in the index.css file for styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
 
 import './App.css'
-import StockBadge from './components/StockBadge/StockBadge'
-import PlanCard from './components/PlanCard/PlanCard'
+
 
 function App() {
 
   return (
     <>
-     <StockBadge productName='Monitor 27""' quantity={25} />
-     <StockBadge productName='Teclado' quantity={0} />
-     <StockBadge productName='Mouse' quantity={6} />
-
-    <PlanCard namePlan='Basico' price={5} isPopular/>
-    <PlanCard namePlan='Full' price={25} isPopular={false}/>
+     <div>Hola mundo</div>
 
     </>
   )
