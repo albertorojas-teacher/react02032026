a7f2d1108ce476eddbeb17a32f3d57fc55e28ef9
Alberto Rojas Gallegos <albertorojas@profesorado.cei.es>
2026-03-03 21:21:31 +0100

    feat: implement Counter component and remove Notifications from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 
 import './App.css'
-import Notifications from './components/Notifications/Notifications'
+import Counter from './components/Counter/Counter'
 
 
 function App() {
 
   return (
     <>
-  <Notifications type='success' message='Usuario Creado' />
-  <Notifications type='alert' message='No hay gasolina' />
-  <Notifications type='error' message='No se pudo guardar en la base de datos' />
+      <Counter />
     </>
   )
 }
